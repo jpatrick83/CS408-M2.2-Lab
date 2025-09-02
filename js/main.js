495b1d80c59d01d50c7b2b76f2f270207c7c7ccf
John <johnpatrick@u.boisestate.edu>
2025-09-02 10:13:37 -0600

    add updated index and image

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@
  */
 
 /**
- * This function prints the string 'Hello World' to the console
+ * This function prints the string 'Hello World' as an alert
  */
 export function helloWorld() {
-    console.log('Hello World');
+    alert('Hello World');
 }
 
 /**
